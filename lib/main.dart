511cc723b8792a1c9b5f8e7cf3f75eeff55625b3
m7med <m7medyt2017@gmail.com>
2024-07-20 21:26:35 +0300

    added local and splash screen and provider and style .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todoapp/pages/home_screen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:todoapp/providers/app_config.dart';
+
 
 void main (){
-  runApp(TodoApp());
+  runApp(ChangeNotifierProvider(create: (context) => AppConfigProvider(), child: const TodoApp(), ));
 }
 
 class TodoApp extends StatelessWidget {
@@ -10,12 +14,17 @@ class TodoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<AppConfigProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.routeName,
       routes: {
         HomeScreen.routeName : (context) => HomeScreen(),
       },
+      themeMode: ThemeMode.dark,
+      locale: Locale(provider.appLanguage),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
     );
   }
 }
